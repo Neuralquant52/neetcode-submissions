af9b90a2e2fa9fe07cc6a08fff7f2710826d2f8f
Neuralquant52 <jg9n7z8t9h@privaterelay.appleid.com>
2026-07-19 01:30:12 +0200

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        for (int i{1}; i < prices.size(); i++){
+            int curr_profit = prices[i] - prices[i - 1];
+            if (curr_profit <= 0) continue;
+            else profit += curr_profit;
+        }
+        return profit;
+    }
+};
